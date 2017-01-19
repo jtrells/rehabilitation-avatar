7dcc664e575ec7befe72c57c79e4fcc0c94d7b1e
juan <juantrelles@gmail.com>
2017-01-18 22:10:09 -0600

    menu

---
+++ Assets/Scripts/VirtualObjectSizeMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class VirtualObjectSizeMenu : MonoBehaviour {
+
+    public Text objectSizeLabel;
+
+    protected float lastButtonUpdateTime = 0f;
+    protected float antiBouncing = 0.4f;
+
+    void Update () {
+        int size = int.Parse(objectSizeLabel.text);
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (ControlBouncing()) size--;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (ControlBouncing()) size++;
+        }
+
+
+        objectSizeLabel.text = size.ToString();
+        PlayerPrefs.SetInt("random_objects_size", size);
+    }
+
+    private bool ControlBouncing()
+    {
+        if (lastButtonUpdateTime + antiBouncing < Time.time)
+        {
+            lastButtonUpdateTime = Time.time;
+            return true;
+        }
+        else return false;
+    }
+}
